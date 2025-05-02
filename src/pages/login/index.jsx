078e7e7ac0daa3e78b6ed1eec85e80018869e119
Julio Reipert <160739776+JulioReipert@users.noreply.github.com>
2025-05-02 16:22:01 -0300

    Delete src directory

--- src/pages/login/index.jsx
+++
@@ -1,9 +0,0 @@
-import "./index.scss";
-
-export default function Login() {
-  return (
-    <div className="page-login">
-      <h1>Login</h1>
-    </div>
-  );
-}
--- src/pages/notFound/index.jsx
+++
@@ -1,9 +0,0 @@
-import "./index.scss";
-
-export default function NotFound() {
-  return (
-    <div className="page-not-found">
-      <h1>Página não encontrada</h1>
-    </div>
-  );
-}
